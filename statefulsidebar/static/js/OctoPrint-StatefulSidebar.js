e8485b20b9f1a87203f92af8a4c1e1db99fa61de
jneilliii <jneilliii+github@gmail.com>
2019-01-10 00:03:29 -0500

    0.1.1

--- statefulsidebar/static/js/OctoPrint-StatefulSidebar.js
+++ statefulsidebar/static/js/OctoPrint-StatefulSidebar.js
@@ -7,18 +7,24 @@
 $(function() {
     function StatefulSidebarViewModel(parameters) {
         var self = this;
-		
+
 		self.settingsViewModel = parameters[0];
 		self.collapsed = ko.observableArray();
-		
+		self.touchui = parameters[1];
+
+		// don't load when touchui is active.
+		if (self.touchui && self.touchui.isActive()) {
+			return;
+		}
+
 		self.onBeforeBinding = function(){
 			self.collapsed(self.settingsViewModel.settings.plugins.statefulsidebar.collapsed());
 			$.each(self.collapsed(),function( index, value ){
 				$('div'+value).removeClass('overflow_visible in').addClass('collapse').siblings('div.accordion-heading').children('a.accordion-toggle').addClass('collapsed');
 			});
 		}
-		
-		self.onStartupComplete = function(){				
+
+		self.onStartupComplete = function(){
 			// update stored settings on hidden/shown events
 			$('div.accordion-body').on('hidden shown',function(){
 				self.collapsed($('a.accordion-toggle.collapsed').map(function() { return $(this).attr('data-target'); }).get());
@@ -29,7 +35,8 @@ $(function() {
 
     OCTOPRINT_VIEWMODELS.push({
         construct: StatefulSidebarViewModel,
-        dependencies: ['settingsViewModel'],
+        dependencies: ['settingsViewModel','touchUIViewModel'],
+		optional: ['touchUIViewModel'],
         elements: []
     });
 });
