c3052146e00bb17cf952ffcaaa6fe3c1043b1286
Martin R. Hufsky <martin@malamaglo.de>
2021-04-23 18:59:04 +0200

    Support generated sidebars
    e.g. webcam sidebar from ui customizer

--- statefulsidebar/static/js/OctoPrint-StatefulSidebar.js
+++ statefulsidebar/static/js/OctoPrint-StatefulSidebar.js
@@ -19,12 +19,13 @@ $(function() {
 
 		self.onBeforeBinding = function(){
 			self.collapsed(self.settingsViewModel.settings.plugins.statefulsidebar.collapsed());
-			$.each(self.collapsed(),function( index, value ){
-				$('div'+value).removeClass('overflow_visible in').addClass('collapse').siblings('div.accordion-heading').children('a.accordion-toggle').addClass('collapsed');
-			});
 		}
 
 		self.onStartupComplete = function(){
+			// restore last state
+			$.each(self.collapsed(),function( index, value ){
+				$('div'+value).removeClass('overflow_visible in').addClass('collapse').siblings('div.accordion-heading').children('a.accordion-toggle').addClass('collapsed');
+			});
 			// update stored settings on hidden/shown events
 			$('div.accordion-body').on('hidden shown',function(){
 				self.collapsed($('a.accordion-toggle.collapsed').map(function() { return $(this).attr('data-target'); }).get());
